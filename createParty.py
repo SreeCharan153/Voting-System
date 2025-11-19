9adff266073bc4158db18035b328c5f4b08423ac
SreeCharan153 <223j1a4632@raghuinstech.com>
2025-11-19 19:10:24 +0530

    Enhance party creation functionality to include president and candidate details

--- createParty.py
+++ createParty.py
@@ -3,9 +3,14 @@ class addparty:
         self.party_name = party_name
 
     
-    def create_party(self):
-        with open("./partys/parties.txt", "a") as file:
-            file.write(f"{self.party_name}\n")
+    def create_party(self,party_prisident, party_candidate):
+        with open("./partys/parties.txt", "r+") as file:
+            lines = file.readlines()
+            last_line = lines[-1]
+            parts = last_line.strip().split(":")
+            party_id = int(parts[0]) + 1
+            file.write(f"{party_id}:{self.party_name}:0\n")
         with open(f"./partys/{self.party_name}.txt", "w") as file:
-            file.write(f"Votes: {self.party_name} : 0\n")
+            file.write(f"Party President:{party_prisident}\n")
+            file.write(f"Party Candidate:{party_candidate}\n")
         return f"Party '{self.party_name}' created successfully."
